951dddf78fa021587489e9c1021edf56b42bdcb7
Angelo Di Martino <aadimartino.18@est.ucab.edu.ve>
2025-06-24 23:55:08 -0400

    validaciones
    Validaciones a nivel de DTO y a nivel de servicio

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Body() dto: { correo: string; contraseña: string }) {
+  login(
+    @Body(new ValidationPipe({ transform: true })) dto: LoginDto //Valido con el dto que el correo y la contraseña tengan un formato correcto
+  ) {
     return this.authService.login(dto.correo, dto.contraseña); // Genera el token para futuras consultas a la API. Dentro tiene el id y el rol del usuario
   }
 }
