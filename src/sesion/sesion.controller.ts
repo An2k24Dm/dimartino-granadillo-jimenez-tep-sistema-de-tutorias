2c7195c50e918c22d61fe980202ddc6173e992f5
Angelo Di Martino <aadimartino.18@est.ucab.edu.ve>
2025-07-09 13:18:04 -0400

    Delete del CRUD de sesion

--- src/sesion/sesion.controller.ts
+++ src/sesion/sesion.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, UseGuards, Patch, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, UseGuards, Patch, Param, ParseIntPipe, Delete, HttpCode, Put, Body } from '@nestjs/common';
 import { SesionService } from './sesion.service';
 import { RolFlexibleGuard } from '../common/guards/rol_flexible.guard';
 import { AllowedRoles } from '../common/decorators/roles_permitidos.decorator';
 import { User } from '../common/decorators/usuario.decorator';
+import { ActualizarSesionDto } from './dto/actualizar_sesion.dto';
 
 @Controller('sesiones')
 export class SesionController {
@@ -23,5 +24,28 @@ export class SesionController {
         @User() usuarioPayload: { userId: number; rol: string },
     ) {
         return this.sesionService.marcarSesionCompletada(sesionId, usuarioPayload.userId);
+    }
+
+    @UseGuards(RolFlexibleGuard)
+    @AllowedRoles('coordinador')
+    @Delete(':id')
+    @HttpCode(200)
+    async eliminarSesion(@Param('id', ParseIntPipe) sesionId: number) {
+    await this.sesionService.eliminarSesion(sesionId);
+    return { message: 'Sesión eliminada correctamente.' };
+    }
+
+    @UseGuards(RolFlexibleGuard)
+    @AllowedRoles('coordinador')
+    @Put(':id')
+    async actualizarSesion(
+        @Param('id', ParseIntPipe) sesionId: number,
+        @Body() dto: ActualizarSesionDto,
+    ) {
+        const sesionActualizada = await this.sesionService.actualizarSesion(sesionId, dto);
+        return {
+        message: 'Sesión actualizada correctamente.',
+        sesion: sesionActualizada,
+        };
     }
 }
