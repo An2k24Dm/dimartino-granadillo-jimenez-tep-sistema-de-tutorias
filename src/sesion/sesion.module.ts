911c957f88864bf309ada09126198e043f6a6e86
Angelo Di Martino <aadimartino.18@est.ucab.edu.ve>
2025-07-09 13:20:07 -0400

    Import del modulo solicitud en sesion

--- src/sesion/sesion.module.ts
+++ src/sesion/sesion.module.ts
@@ -5,9 +5,10 @@ import { SesionService } from './sesion.service';
 import { SesionController } from './sesion.controller';
 import { SolicitudModule } from '../solicitud/solicitud.module'; // si usas servicio de solicitud
 import { AuthModule } from '../auth/auth.module';
+import { Solicitud } from 'src/solicitud/solicitud.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sesion]), SolicitudModule,AuthModule],
+  imports: [TypeOrmModule.forFeature([Sesion, Solicitud]), SolicitudModule,AuthModule],
   controllers: [SesionController],
   providers: [SesionService],
 })
